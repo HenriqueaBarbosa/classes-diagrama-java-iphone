176cb0306a71cd1c625d9b1727b1e98edb43b3c0
Henrique Barbosa <henrique.abdiel@gmail.com>
2023-08-11 13:46:03 -0300

    feat: diagrmação e classes java do Iphone

---
+++ src/AparelhoTelefonico.java
@@ -0,0 +1,8 @@
+public interface AparelhoTelefonico {
+
+    public abstract void ligar();
+
+    public abstract void atender();
+
+    public abstract void iniciarCorreioVoz();
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Iphone iPhone = new Iphone();
+
+        iPhone.tocar();
+        iPhone.pausar();
+        iPhone.selecionarMusica();
+        iPhone.ligar();
+        iPhone.atender();
+        iPhone.iniciarCorreioVoz();
+        iPhone.exibirPagina();
+        iPhone.adicionarNovaAba();
+        iPhone.atualizarPagina();
+    }
+}
